2b8cd693350c19345d368fe3dd668f75f2101416
Chris Ellis <ce8958@gmail.com>
2022-07-22 18:12:37 +0100

    feat: started implementing POST mapping to add ticket

--- src/main/java/com/ticketserver/ticketserver/ApiController.java
+++ src/main/java/com/ticketserver/ticketserver/ApiController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping(path = "/")
@@ -16,7 +18,7 @@ public class ApiController {
     @Autowired
     private TicketRepository ticketRepository;
 
-    @GetMapping(path = "/all/users")
+    @GetMapping(path = "/users/all")
     public @ResponseBody Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
@@ -26,7 +28,7 @@ public class ApiController {
         return userRepository.findUserByUserName(name);
     }
 
-    @GetMapping(path = "/all/tickets")
+    @GetMapping(path = "/tickets/all")
     public @ResponseBody Iterable<Ticket> getAllTickets() {
         return ticketRepository.findAll();
     }
@@ -38,5 +40,18 @@ public class ApiController {
         userRepository.save(user);
         return "User added";
     }
+
+    /*
+     * Figure out how to find userID and add a ticket that links to correct user, ?set so users have to have unique username then find userID by username
+     */
+    @PostMapping(value="/ticket/add")
+    public @ResponseBody String postMethodName(@RequestParam String userName, String content) {
+        Ticket ticket = new Ticket();
+        ticket.setContent(content);
+        ticketRepository.save(ticket);
+        
+        return "Ticket added";
+    }
+    
 
 }
